6fe369ec7e8f50b49fb1b804d932397dc969e57e
Marco Hemken <mehemken@gmail.com>
2017-01-22 11:07:57 -0800

    final edits

--- french_deck.py
+++ french_deck.py
@@ -10,14 +10,14 @@ class FrenchDeck:
         self._cards = [Card(rank, suit) for suit in self.suits
                                         for rank in self.ranks]
 
-    def __len__(self):
-        return len(self._cards)
+    # def __len__(self):
+        # return len(self._cards)
 
-    def __getitem__(self, position):
-        return self._cards[position]
+    # def __getitem__(self, position):
+        # return self._cards[position]
 
-    def __setitem__(self, position, card):
-        self._cards[position] = card
+    # def __setitem__(self, position, card):
+        # self._cards[position] = card
 
 
 # collections module
